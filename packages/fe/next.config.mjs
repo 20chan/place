89fa0657cd4ff98c7630f5594aad8ad04c39e27c
20chan <2@0chan.dev>
2024-04-26 00:49:37 +0900

    can run next dev and prod

--- packages/fe/next.config.mjs
+++ packages/fe/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = (phase) => {
+  const isProd = (
+    process.env.NODE_ENV === 'production'
+    || phase === 'phase-production-build'
+    || phase === 'phase-production-server'
+  );
+
+  return {
+    distDir: isProd ? '.next' : '.next.dev',
+  }
+};
 
 export default nextConfig;
